6ce2f95a06f27340b01e8da0b8447a029175f5d0
Andy Guinder <andreabelen.guinder@gmail.com>
2026-09-15 19:09:48 -0300

    mod bd

--- models/bd.js
+++ models/bd.js
@@ -7,7 +7,7 @@ var pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB_NAME,
-  port: process.env.MYSQL_PORT || 3306,
+  port: parseInt(process.env.MYSQL_PORT || '3306', 10),
   ssl: process.env.MYSQL_HOST !== 'localhost' ? { rejectUnauthorized: false } : false
 });
 
